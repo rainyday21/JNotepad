1e47490dd9d4c9679d1718159b4f2a7d905fda48
rainyday21 <rainydayscholar21@gmail.com>
2021-12-12 21:31:03 -0800

    Implementing node to find and find next dialog

---
+++ Node.java
@@ -0,0 +1,16 @@
+public class Node<T> {
+	  private T    data; // Entry in bag
+	  private Node next; // Link to next node
+
+		private Node(T dataPortion)
+		{
+			this(dataPortion, null);	
+		} // end constructor
+		
+		private Node(T dataPortion, Node nextNode)
+		{
+			data = dataPortion;
+			next = nextNode;	
+		} // end constructor
+	} // end Node
+
